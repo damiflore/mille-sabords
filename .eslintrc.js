105cca02d2bb9b0974fe6c3142342bc4535386f3
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-08 12:13:48 +0200

    fix eslint syntax error on jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
 const { createConfig } = require("@jsenv/eslint-config")
 
-const config = createConfig()
-config.rules["import/no-absolute-path"] = ["off"]
-config.settings["import/resolver"] = {
-  [`${__dirname}/node_modules/@jsenv/eslint-import-resolver/dist/commonjs/main.js`]: {
-    projectPath: __dirname,
-  },
-}
-config.parserOptions.ecmaFeatures.jsx = true
-config.plugins.push("react")
-config.settings.react = {
-  pragma: "createElement",
-}
-config.extends = ["plugin:react/recommended"]
-Object.assign(config.rules, {
-  "react/prop-types": ["off"],
-  "react/jsx-no-target-blank": ["off"],
-})
+const config = createConfig({ projectPath: __dirname, jsxEnabled: true, reactPluginEnabled: true })
 
 module.exports = config
