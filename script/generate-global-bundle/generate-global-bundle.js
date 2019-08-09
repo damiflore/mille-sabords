1ef8f819c3487b279dc55dfecbe8ab737ed0517a
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-09 16:46:10 +0200

    add todo comment about using systemjs format

--- script/generate-global-bundle/generate-global-bundle.js
+++ script/generate-global-bundle/generate-global-bundle.js
@@ -1,6 +1,10 @@
 const { generateGlobalBundle } = require("@jsenv/bundling")
 const { projectPath, babelPluginMap, convertMap } = require("../../jsenv.config.js")
 
+// TODO: change this for system format
+// so that we can use dynamic import and benefit
+// from code splitting
+
 // this is to get the production build of react
 process.env.NODE_ENV = "production"
 
