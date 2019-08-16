6ce59405458ed770d5b0261b7b7504156db2b564
Damien Maillard <dmaillard06@gmail.com>
2019-08-16 18:07:59 +0200

    write tests for computeScore

---
+++ test/computeScore/sword-challenge.js
@@ -0,0 +1,33 @@
+import { assert } from "@dmail/assert"
+
+const computeScore = () => {}
+
+// 8 coin
+{
+  const actual = computeScore({
+    currentCard: { type: "sword-challenge", goal: 2, gamble: 300 },
+    diceKept: ["coin", "coin", "coin", "coin", "coin", "coin", "coin", "coin"],
+  })
+  const expected = -300
+  assert({ actual, expected })
+}
+
+// 2 sword completed with 2 sword
+{
+  const actual = computeScore({
+    currentCard: { type: "sword-challenge", goal: 2, gamble: 300 },
+    diceKept: ["sword", "sword", "monkey", "monkey", "skull", "skull", "parrot", "parrot"],
+  })
+  const expected = 300
+  assert({ actual, expected })
+}
+
+// 2 sword completed with 3 sword
+{
+  const actual = computeScore({
+    currentCard: { type: "sword-challenge", goal: 2, gamble: 300 },
+    diceKept: ["sword", "sword", "sword", "monkey", "skull", "skull", "parrot", "parrot"],
+  })
+  const expected = 400
+  assert({ actual, expected })
+}
