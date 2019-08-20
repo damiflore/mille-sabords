7651deedc611c619a44a80144175ae8b10b08fe4
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-20 11:51:02 +0200

    update lighthouse ci script

--- script/lighthouse-ci/lighthouse-ci.js
+++ script/lighthouse-ci/lighthouse-ci.js
@@ -3,20 +3,27 @@ const { exec } = require("child_process")
 const run = async () => {
   process.env.PORT = 8080
   const { serverPromise } = require("../start/start.js")
+  console.log("process.env.TRAVIS_PULL_REQUEST", process.env.TRAVIS_PULL_REQUEST)
 
   const server = await serverPromise
   try {
     await new Promise((resolve, reject) => {
-      exec("npm run lighthousebot", (error, stdout) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(stdout)
-        }
-      })
+      exec(
+        "npm run lighthousebot",
+        {
+          env: process.env,
+        },
+        (error, stdout) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(stdout)
+          }
+        },
+      )
     })
   } finally {
-    server.stop()
+    server.stop("lighthouse report done")
   }
 }
 run()
