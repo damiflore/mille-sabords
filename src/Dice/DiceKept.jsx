90ef570bf53dda81662c43ee6688398037594624
Flore Giraudier <fgiraudier@vulog.local>
2020-05-22 13:42:52 +0200

    add cursed cover on the box when 3 skulls

--- src/Dice/DiceKept.jsx
+++ src/Dice/DiceKept.jsx
@@ -2,13 +2,20 @@ import React from "react"
 import { Dice } from "./Dice.jsx"
 import { useGameStore } from "src/MilleSabordGame.js"
 import { unkeepDice } from "src/game.actions.js"
-import { useUnkeepDiceAllowed } from "src/game.selectors.js"
+import { useUnkeepDiceAllowed, useMarkScorePermission } from "src/game.selectors.js"
 import { RoundScore } from "src/Score/RoundScore.jsx"
+import { HAS_THREE_SKULLS_OR_MORE } from "src/constants.js"
 
 export const DiceKept = () => {
   const store = useGameStore()
-  const { diceKept } = store
+  const { diceKept, isOnSkullIsland } = store
   const unkeepDiceAllowed = useUnkeepDiceAllowed()
+  const markScorePermission = useMarkScorePermission(store)
+
+  const roundOver =
+    markScorePermission.reason === HAS_THREE_SKULLS_OR_MORE ||
+    isOnSkullIsland ||
+    !markScorePermission.allowed
 
   return (
     <div className="dice-kept">
@@ -30,6 +37,13 @@ export const DiceKept = () => {
         <div className="top-right-corner"></div>
         <div className="bottom-left-corner"></div>
         <div className="bottom-right-corner"></div>
+        {roundOver && (
+          <>
+            <div className="cursed-cover">
+              <img src={`src/Dice/assets/cursed-cover.png`} alt="cursed-cover" />
+            </div>
+          </>
+        )}
       </div>
       <RoundScore />
     </div>
