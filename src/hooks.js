824d0dc9b7905e8b95ab5a5151856b73267f71c4
Damien Maillard <damien.maillard@dailymotion.com>
2020-05-25 09:21:17 +0200

    fix useBecomes hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,33 +1,25 @@
 import React from "react"
 
-const { useEffect, useRef, useState } = React
+const { useEffect, useRef } = React
 
-export const usePrevious = (value) => {
-  const ref = useRef()
-  ref.current = value
+// https://stackoverflow.com/a/61680184/2634179
+export const useBecomes = (callback, deps) => {
+  const previousDeps = usePrevious(deps)
+
+  const mountedRef = useRef(false)
+  useEffect(() => {
+    if (mountedRef.current === false) {
+      mountedRef.current = true
+    }
+  })
+
+  return mountedRef.current ? Boolean(callback(...previousDeps)) : false
+}
+
+const usePrevious = (value) => {
+  const ref = useRef(value)
   useEffect(() => {
     ref.current = value
   }, [value])
   return ref.current
-}
-
-// https://stackoverflow.com/a/61680184/2634179
-export const useBecomes = (callback, deps) => {
-  const [becomes, setBecomes] = useState(false)
-
-  const argsRef = useRef(null)
-  useEffect(() => {
-    if (argsRef.current !== null) {
-      const returnValue = callback(...argsRef.current)
-      if (returnValue) {
-        // tell outside it becomes true
-        setBecomes(true)
-        // immediatly reset to false
-        setBecomes(false)
-      }
-    }
-    argsRef.current = deps
-  }, deps)
-
-  return becomes
 }
