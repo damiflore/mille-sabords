d1769d56ce50dd477f6a96332d3f60d676fbfb10
Damien Maillard <damien.maillard@dailymotion.com>
2020-05-26 10:56:55 +0200

    Add comment about useMemo

--- src/game.component.js
+++ src/game.component.js
@@ -17,18 +17,29 @@ export const diceRolledAreaElementRef = createRef()
 export const Game = ({ gameState }) => {
   return (
     <GameContextProvider initialState={gameState}>
-      {useMemo(() => (
-        <>
-          <GameEffects />
-          <Header />
-          <div className="dice-kept-and-skulls">
-            <DiceKept />
-            <SkullIsland />
-          </div>
-          <DiceOnGoing ref={diceRolledAreaElementRef} />
-          <Footer />
-        </>
-      ))}
+      {
+        /*
+        useMemo usage below means the components won't be re-rendered when game global state changes
+        and it's fine because as you can see component structure is not conditioned by the gameState or anything.
+        Every descendant will still be re-rendered by react and if some component are expensive to render
+        they can be wrapped by useMemo with the same pattern.
+        (Don't forget to pass dependencies as second arg if there is any).
+
+        There is no real need for useMemo here: it's kept as an example.
+        */
+        useMemo(() => (
+          <>
+            <GameEffects />
+            <Header />
+            <div className="dice-kept-and-skulls">
+              <DiceKept />
+              <SkullIsland />
+            </div>
+            <DiceOnGoing ref={diceRolledAreaElementRef} />
+            <Footer />
+          </>
+        ))
+      }
     </GameContextProvider>
   )
 }
