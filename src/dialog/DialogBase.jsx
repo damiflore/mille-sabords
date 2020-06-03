aaf0318fb4743c61a1924a66f8e9ed6c35be1cdf
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-03 17:33:17 +0200

    prevent drag to scroll on touch when dialog is opened

--- src/dialog/DialogBase.jsx
+++ src/dialog/DialogBase.jsx
@@ -20,6 +20,8 @@ const OVERLAY_STYLE = {
   right: 0,
   bottom: 0,
   backgroundColor: "rgba(255, 255, 255, 0.75)",
+  // prevent drag to scroll on the overlay
+  touchAction: "none",
 }
 
 const DIALOG_STYLE = {
