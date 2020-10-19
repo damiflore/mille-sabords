766d74bda46942a3347913230e33715acdce94b1
Damien Maillard <damien.maillard@dailymotion.com>
2020-10-19 17:40:36 +0200

    update dist

---
+++ dist/systemjs/index.js-17a525c2.js
@@ -0,0 +1,2 @@
+System.register(["./index-eeafc199.js"],(function(){"use strict";return{setters:[function(){}],execute:function(){}}}));
+//# sourceMappingURL=index.js-17a525c2.js.map
--- dist/systemjs/index.js-7b12a85d.js
+++
@@ -1,2 +0,0 @@
-System.register(["./index-e17d71b6.js"],(function(){"use strict";return{setters:[function(){}],execute:function(){}}}));
-//# sourceMappingURL=index.js-7b12a85d.js.map
