6ba75c3313947ad8bd4712303f46e4cba8afc442
Damien Maillard <damien.maillard@dailymotion.com>
2020-11-19 07:00:32 +0100

    add diamond effect

--- src/round/useRoundScoreParticleEffects.js
+++ src/round/useRoundScoreParticleEffects.js
@@ -4,25 +4,12 @@ import { getDomNodeRectangle } from "src/dom/dom.position.js"
 import { getRectangleCenterPoint } from "src/helper/rectangle.js"
 import { useChestSlots, useDices } from "src/main.store.js"
 import { chestSlotContentToSymbol, compareChestSlotContent } from "src/chest/chest.jsx"
-import { symbolIsCoin } from "src/symbols/symbols.js"
+import { SYMBOL_COIN, SYMBOL_DIAMOND } from "src/symbols/symbols.js"
 
 export const useRoundScoreParticleEffects = ({ addScoreParticle }) => {
-  useCoinEffect({ addScoreParticle })
-  useComboEffect({ addScoreParticle })
-}
-
-const useCoinEffect = ({ addScoreParticle }) => {
-  const chestSlotUpdates = useChestSlotUpdatesWithoutMoves()
-  const dices = useDices()
-  const updatesWithCoins = React.useMemo(() => {
-    return chestSlotUpdates.filter(({ chestSlotContent }) => {
-      const chestSlotSymbol = chestSlotContentToSymbol(chestSlotContent, dices)
-      return chestSlotSymbol && symbolIsCoin(chestSlotSymbol)
-    })
-  }, [chestSlotUpdates, dices])
-
-  useUpdateEffect(() => {
-    const cleanupFunctions = updatesWithCoins.map(({ chestSlot }) => {
+  useSymbolEffect({
+    symbol: SYMBOL_COIN,
+    symbolEffect: ({ chestSlot }) => {
       return addScoreParticle({
         id: `chest-slot-${chestSlot}-coin`,
         value: 100,
@@ -30,13 +17,46 @@ const useCoinEffect = ({ addScoreParticle }) => {
           document.querySelector(`[data-chest-slot="${chestSlot}"]`),
         ),
       })
+    },
+  })
+  useSymbolEffect({
+    symbol: SYMBOL_DIAMOND,
+    symbolEffect: ({ chestSlot }) => {
+      return addScoreParticle({
+        id: `chest-slot-${chestSlot}-diamond`,
+        value: 100,
+        ...chestSlotDomNodeToScoreParticlePosition(
+          document.querySelector(`[data-chest-slot="${chestSlot}"]`),
+        ),
+      })
+    },
+  })
+
+  useComboEffect({ addScoreParticle })
+}
+
+const useSymbolEffect = ({ symbol, symbolEffect }) => {
+  const chestSlotUpdates = useChestSlotUpdatesWithoutMoves()
+  const dices = useDices()
+  const updatesWithThatSymbol = React.useMemo(() => {
+    return chestSlotUpdates.filter(({ chestSlotContent }) => {
+      const chestSlotSymbol = chestSlotContentToSymbol(chestSlotContent, dices)
+      return chestSlotSymbol && chestSlotSymbol === symbol
+    })
+  }, [symbol, chestSlotUpdates, dices])
+
+  useUpdateEffect(() => {
+    const effectReturnValues = updatesWithThatSymbol.map((updateWithThatSymbol) => {
+      return symbolEffect(updateWithThatSymbol)
     })
     return () => {
-      cleanupFunctions.forEach((cleanup) => {
-        cleanup()
+      effectReturnValues.forEach((effectReturnValue) => {
+        if (typeof effectReturnValue === "function") {
+          effectReturnValue()
+        }
       })
     }
-  }, [updatesWithCoins])
+  }, [updatesWithThatSymbol])
 }
 
 const useComboEffect = ({ addScoreParticle }) => {
