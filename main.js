02d93d4bd8cf71c358baea97db236d190743c0fc
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-03 08:23:27 +0100

    start tesing service worker on exploring

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+import { registerServiceWorker } from "@jsenv/pwa"
 import { createMilleSabordGame } from "./createMilleSabordGame.js"
 
 createMilleSabordGame({
   into: document.querySelector("#app"),
 })
+
+registerServiceWorker("/service-worker.js")
--- service-worker.js
+++ service-worker.js
@@ -5,5 +5,6 @@ self.importScripts("./node_modules/@jsenv/pwa/src/service-worker.setup.js")
 config.cacheName = "mille-sabords"
 config.urlMap = { "/": "main.prod.html" }
 config.extraUrlsToCacheOnInstall = []
+config.disableNavigationPreload = true
 
 self.importScripts("./node_modules/@jsenv/pwa/src/service-worker.main.js")
