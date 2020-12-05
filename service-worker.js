cd2b24cc464a6fe118a764ecc25b94ca02ebafc2
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-05 17:56:41 +0100

    mille sabord fonctionne hors ligne, youpi

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,6 @@
 self.importScripts("./node_modules/@jsenv/pwa/src/service-worker.setup.js")
 
 config.cacheName = "mille-sabords"
-config.urlMap = { "/": "main.prod.html" }
-config.extraUrlsToCacheOnInstall = []
 
 self.importScripts("./node_modules/@jsenv/pwa/src/service-worker.main.js")
 
