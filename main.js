e4a9f617691eccadd297ad375447e9b49e561ea5
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-06 16:05:04 +0100

    register service worker later

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-import { registerServiceWorker } from "@jsenv/pwa"
 import { createMilleSabordGame } from "./createMilleSabordGame.js"
 
 createMilleSabordGame({
   into: document.querySelector("#app"),
 })
-
-registerServiceWorker("/service-worker.js")
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { registerServiceWorker } from "@jsenv/pwa"
 import { useGameCreated } from "src/main.store.js"
 import { useMainDomNodeSetter } from "src/dom/dom.main.js"
 import { Home } from "src/home/Home.jsx"
@@ -9,6 +10,10 @@ import { Settings } from "src/settings/settings.component.js"
 import milleSabordsCssUrl from "../mille-sabord.css"
 
 export const App = (props) => {
+  React.useEffect(() => {
+    registerServiceWorker("/service-worker.js")
+  }, [])
+
   return (
     <div id="main-container">
       <div id="main" ref={useMainDomNodeSetter()}>
