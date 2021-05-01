79b677c7b25ecc643e4aaf16e1f4d45d094bcc8b
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-01 10:42:00 +0200

    update eslint config and some deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,50 @@
 const { createEslintConfig } = require("@jsenv/eslint-config")
 
-const config = createEslintConfig({
+const eslintConfig = createEslintConfig({
   projectDirectoryUrl: __dirname,
   importResolutionMethod: "import-map",
   importMapFileRelativeUrl: "./importmap.dev.importmap",
+  browser: true,
+  html: true,
+  prettier: true,
   react: true,
   jsx: true,
 })
 
-module.exports = config
+const importResolverPath = require.resolve("@jsenv/importmap-eslint-resolver")
+// consider some files inside as written for Node.js
+eslintConfig.overrides.push({
+  files: [".github/**/*.js", "script/**/*.js", "jsenv.config.js"],
+  env: {
+    es6: true,
+    browser: false,
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      [importResolverPath]: {
+        node: true,
+      },
+    },
+  },
+})
+
+// any file ending with .cjs as written for Node.js with commonjs module resolution
+eslintConfig.overrides.push({
+  files: ["**/*.cjs"],
+  env: {
+    es6: true,
+    browser: false,
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      [importResolverPath]: {
+        node: true,
+        commonJsModuleResolution: true,
+      },
+    },
+  },
+})
+
+module.exports = eslintConfig
