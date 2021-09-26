06a470c757d1c63ea4468873532786a493e87524
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-26 17:23:40 +0200

    update dist

--- dist/systemjs/app2-36767d20.js
+++
@@ -1,2 +0,0 @@
-System.register(["./app.js"],(function(e){"use strict";return{setters:[function(t){e("createMilleSabordGame",t.au)}],execute:function(){}}}));
-//# sourceMappingURL=app2-36767d20.js.map
