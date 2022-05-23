79b630caadd315bbe0ca9e5fdffe39a0218c1ee4
dmail <dmaillard06@gmail.com>
2022-05-23 15:50:13 +0200

    stylesheet now loaded by app loader

--- src/app/root.jsx
+++ src/app/root.jsx
@@ -4,15 +4,11 @@ import { useGameCreated } from "/src/app/main.store.js"
 import { useMainDomNodeSetter } from "/src/app/dom/dom.main.jsx"
 import { Home } from "/src/app/home/Home.jsx"
 import { Game } from "/src/app/game/Game.jsx"
-import { Stylesheet } from "/src/app/generic/Stylesheet.jsx"
 import { Settings } from "/src/app/settings/settings.component.jsx"
-
-const appCssUrl = new URL("./app.css", import.meta.url)
 
 export const App = (props) => {
   return (
     <div id="main" ref={useMainDomNodeSetter()}>
-      <Stylesheet href={appCssUrl} />
       <Settings />
       <AppBody {...props} />
     </div>
