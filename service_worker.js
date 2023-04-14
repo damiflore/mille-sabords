8346177cef41cee018852f0e554a225a594ca145
dmail <dmaillard06@gmail.com>
2023-04-14 08:51:33 +0000

    Deploying to gh-pages from @ damiflore/mille-sabords@b2195964f33ac4e5141f0b65efad12fb959ecb40 🚀

---
+++ service_worker.js
@@ -0,0 +1,270 @@
+
+self.resourcesFromJsenvBuild = {
+  "/mille-sabords/index.html": {
+    "version": "c652b9e7"
+  },
+  "/mille-sabords/other/favicon.png": {
+    "version": "25e95a00",
+    "versionedUrl": "/mille-sabords/other/favicon.png?v=25e95a00"
+  },
+  "/mille-sabords/other/mille-sabords.webmanifest": {
+    "version": "23af43db"
+  },
+  "/mille-sabords/other/pwa_icon_192.png": {
+    "version": "574c1c76",
+    "versionedUrl": "/mille-sabords/other/pwa_icon_192.png?v=574c1c76"
+  },
+  "/mille-sabords/css/app.css": {
+    "version": "31f65f42",
+    "versionedUrl": "/mille-sabords/css/app.css?v=31f65f42"
+  },
+  "/mille-sabords/js/app_loader.nomodule.js": {
+    "version": "fd8505e9",
+    "versionedUrl": "/mille-sabords/js/app_loader.nomodule.js?v=fd8505e9"
+  },
+  "/mille-sabords/js/app.nomodule.js": {
+    "version": "33630b7e",
+    "versionedUrl": "/mille-sabords/js/app.nomodule.js?v=33630b7e"
+  },
+  "/mille-sabords/js/vendors.nomodule.js": {
+    "version": "6729057b",
+    "versionedUrl": "/mille-sabords/js/vendors.nomodule.js?v=6729057b"
+  },
+  "/mille-sabords/other/arrr_matey_bb_wn3.ttf": {
+    "version": "3d20259e",
+    "versionedUrl": "/mille-sabords/other/arrr_matey_bb_wn3.ttf?v=3d20259e"
+  },
+  "/mille-sabords/other/pwa_icon_512.png": {
+    "version": "d2dd96fd",
+    "versionedUrl": "/mille-sabords/other/pwa_icon_512.png?v=d2dd96fd"
+  },
+  "/mille-sabords/other/maskable_icon_192.png": {
+    "version": "c20be9d4",
+    "versionedUrl": "/mille-sabords/other/maskable_icon_192.png?v=c20be9d4"
+  },
+  "/mille-sabords/other/wood-box.jpg": {
+    "version": "fcdd41cc",
+    "versionedUrl": "/mille-sabords/other/wood-box.jpg?v=fcdd41cc"
+  },
+  "/mille-sabords/other/treasure-map.png": {
+    "version": "6ec97baa",
+    "versionedUrl": "/mille-sabords/other/treasure-map.png?v=6ec97baa"
+  },
+  "/mille-sabords/other/metal.jpg": {
+    "version": "36435573",
+    "versionedUrl": "/mille-sabords/other/metal.jpg?v=36435573"
+  },
+  "/mille-sabords/other/pirate-hook.png": {
+    "version": "02fba0b6",
+    "versionedUrl": "/mille-sabords/other/pirate-hook.png?v=02fba0b6"
+  },
+  "/mille-sabords/other/pirate-hook-02.png": {
+    "version": "3db1b696",
+    "versionedUrl": "/mille-sabords/other/pirate-hook-02.png?v=3db1b696"
+  },
+  "/mille-sabords/other/skull-bottle.png": {
+    "version": "7c833fb2",
+    "versionedUrl": "/mille-sabords/other/skull-bottle.png?v=7c833fb2"
+  },
+  "/mille-sabords/other/sea.jpg": {
+    "version": "096e0699",
+    "versionedUrl": "/mille-sabords/other/sea.jpg?v=096e0699"
+  },
+  "/mille-sabords/other/skullsandcrossbones.ttf": {
+    "version": "70af14b1",
+    "versionedUrl": "/mille-sabords/other/skullsandcrossbones.ttf?v=70af14b1"
+  },
+  "/mille-sabords/other/pirate_treasure_demo_4b3mb.otf": {
+    "version": "febb1694",
+    "versionedUrl": "/mille-sabords/other/pirate_treasure_demo_4b3mb.otf?v=febb1694"
+  },
+  "/mille-sabords/other/wood.jpg": {
+    "version": "3c23f120",
+    "versionedUrl": "/mille-sabords/other/wood.jpg?v=3c23f120"
+  },
+  "/mille-sabords/other/card_small-1skull.png": {
+    "version": "3c9f4a9f",
+    "versionedUrl": "/mille-sabords/other/card_small-1skull.png?v=3c9f4a9f"
+  },
+  "/mille-sabords/other/card_small-2skulls.png": {
+    "version": "683eae22",
+    "versionedUrl": "/mille-sabords/other/card_small-2skulls.png?v=683eae22"
+  },
+  "/mille-sabords/other/card_small-sword-challenge.png": {
+    "version": "dc6ca6cf",
+    "versionedUrl": "/mille-sabords/other/card_small-sword-challenge.png?v=dc6ca6cf"
+  },
+  "/mille-sabords/other/card_small-animals.png": {
+    "version": "b931298f",
+    "versionedUrl": "/mille-sabords/other/card_small-animals.png?v=b931298f"
+  },
+  "/mille-sabords/other/card_small-chest.png": {
+    "version": "b171aea9",
+    "versionedUrl": "/mille-sabords/other/card_small-chest.png?v=b171aea9"
+  },
+  "/mille-sabords/other/card_small-coin.png": {
+    "version": "a99cbd8e",
+    "versionedUrl": "/mille-sabords/other/card_small-coin.png?v=a99cbd8e"
+  },
+  "/mille-sabords/other/card_small-diamond.png": {
+    "version": "d3b35497",
+    "versionedUrl": "/mille-sabords/other/card_small-diamond.png?v=d3b35497"
+  },
+  "/mille-sabords/other/card_small-pirate.png": {
+    "version": "ada07362",
+    "versionedUrl": "/mille-sabords/other/card_small-pirate.png?v=ada07362"
+  },
+  "/mille-sabords/other/card_small-witch.png": {
+    "version": "5e50e07c",
+    "versionedUrl": "/mille-sabords/other/card_small-witch.png?v=5e50e07c"
+  },
+  "/mille-sabords/other/card_1skull.png": {
+    "version": "47b3b46a",
+    "versionedUrl": "/mille-sabords/other/card_1skull.png?v=47b3b46a"
+  },
+  "/mille-sabords/other/card_2skulls.png": {
+    "version": "61ce4149",
+    "versionedUrl": "/mille-sabords/other/card_2skulls.png?v=61ce4149"
+  },
+  "/mille-sabords/other/card_2sword-challenge.png": {
+    "version": "5c1924fd",
+    "versionedUrl": "/mille-sabords/other/card_2sword-challenge.png?v=5c1924fd"
+  },
+  "/mille-sabords/other/card_3sword-challenge.png": {
+    "version": "4097c59a",
+    "versionedUrl": "/mille-sabords/other/card_3sword-challenge.png?v=4097c59a"
+  },
+  "/mille-sabords/other/card_4sword-challenge.png": {
+    "version": "f3927ba3",
+    "versionedUrl": "/mille-sabords/other/card_4sword-challenge.png?v=f3927ba3"
+  },
+  "/mille-sabords/other/card_animals.png": {
+    "version": "e97f74ef",
+    "versionedUrl": "/mille-sabords/other/card_animals.png?v=e97f74ef"
+  },
+  "/mille-sabords/other/card_chest.png": {
+    "version": "037b957f",
+    "versionedUrl": "/mille-sabords/other/card_chest.png?v=037b957f"
+  },
+  "/mille-sabords/other/card_coin.png": {
+    "version": "19c1c721",
+    "versionedUrl": "/mille-sabords/other/card_coin.png?v=19c1c721"
+  },
+  "/mille-sabords/other/card_default.png": {
+    "version": "d777c0de",
+    "versionedUrl": "/mille-sabords/other/card_default.png?v=d777c0de"
+  },
+  "/mille-sabords/other/card_diamond.png": {
+    "version": "ae0da860",
+    "versionedUrl": "/mille-sabords/other/card_diamond.png?v=ae0da860"
+  },
+  "/mille-sabords/other/card_pirate.png": {
+    "version": "57c15344",
+    "versionedUrl": "/mille-sabords/other/card_pirate.png?v=57c15344"
+  },
+  "/mille-sabords/other/card_witch.png": {
+    "version": "24c51955",
+    "versionedUrl": "/mille-sabords/other/card_witch.png?v=24c51955"
+  },
+  "/mille-sabords/other/dice_coin.png": {
+    "version": "37896c9d",
+    "versionedUrl": "/mille-sabords/other/dice_coin.png?v=37896c9d"
+  },
+  "/mille-sabords/other/dice_diamond.png": {
+    "version": "6c36ed54",
+    "versionedUrl": "/mille-sabords/other/dice_diamond.png?v=6c36ed54"
+  },
+  "/mille-sabords/other/dice_monkey.png": {
+    "version": "044ce09c",
+    "versionedUrl": "/mille-sabords/other/dice_monkey.png?v=044ce09c"
+  },
+  "/mille-sabords/other/dice_parrot.png": {
+    "version": "21f5c4b5",
+    "versionedUrl": "/mille-sabords/other/dice_parrot.png?v=21f5c4b5"
+  },
+  "/mille-sabords/other/dice_skull.png": {
+    "version": "271c6b85",
+    "versionedUrl": "/mille-sabords/other/dice_skull.png?v=271c6b85"
+  },
+  "/mille-sabords/other/dice_sword.png": {
+    "version": "f0ac96f9",
+    "versionedUrl": "/mille-sabords/other/dice_sword.png?v=f0ac96f9"
+  },
+  "/mille-sabords/other/witch-label.png": {
+    "version": "f95d3d2f",
+    "versionedUrl": "/mille-sabords/other/witch-label.png?v=f95d3d2f"
+  },
+  "/mille-sabords/js/root.nomodule.js": {
+    "version": "a92bbf1e",
+    "versionedUrl": "/mille-sabords/js/root.nomodule.js?v=a92bbf1e"
+  },
+  "/mille-sabords/js/jsenv_service_worker.js": {
+    "version": "9047ac4f",
+    "versionedUrl": "/mille-sabords/js/jsenv_service_worker.js?v=9047ac4f"
+  },
+  "/mille-sabords/css/dialog.base.css": {
+    "version": "11b6a1ee",
+    "versionedUrl": "/mille-sabords/css/dialog.base.css?v=11b6a1ee"
+  },
+  "/mille-sabords/css/dialog.css": {
+    "version": "3ddbc36c",
+    "versionedUrl": "/mille-sabords/css/dialog.css?v=3ddbc36c"
+  },
+  "/mille-sabords/other/cursed-grid.png": {
+    "version": "d5dc17c3",
+    "versionedUrl": "/mille-sabords/other/cursed-grid.png?v=d5dc17c3"
+  },
+  "/mille-sabords/other/swords-disabled.png": {
+    "version": "124616f0",
+    "versionedUrl": "/mille-sabords/other/swords-disabled.png?v=124616f0"
+  },
+  "/mille-sabords/other/win-treasure.png": {
+    "version": "7f39a7fa",
+    "versionedUrl": "/mille-sabords/other/win-treasure.png?v=7f39a7fa"
+  },
+  "/mille-sabords/other/boat.png": {
+    "version": "dc6ca6cf",
+    "versionedUrl": "/mille-sabords/other/boat.png?v=dc6ca6cf"
+  },
+  "/mille-sabords/other/Luffy.png": {
+    "version": "59728a08",
+    "versionedUrl": "/mille-sabords/other/Luffy.png?v=59728a08"
+  },
+  "/mille-sabords/other/miss_fortune.png": {
+    "version": "4b135ade",
+    "versionedUrl": "/mille-sabords/other/miss_fortune.png?v=4b135ade"
+  },
+  "/mille-sabords/other/JackSparrow.png": {
+    "version": "f439b5cc",
+    "versionedUrl": "/mille-sabords/other/JackSparrow.png?v=f439b5cc"
+  },
+  "/mille-sabords/other/BarbeRouge.png": {
+    "version": "b4a9d8e9",
+    "versionedUrl": "/mille-sabords/other/BarbeRouge.png?v=b4a9d8e9"
+  },
+  "/mille-sabords/other/GinetteBoulette.png": {
+    "version": "de6a1ce1",
+    "versionedUrl": "/mille-sabords/other/GinetteBoulette.png?v=de6a1ce1"
+  },
+  "/mille-sabords/other/CapitaineCrochet.png": {
+    "version": "0ce0da4f",
+    "versionedUrl": "/mille-sabords/other/CapitaineCrochet.png?v=0ce0da4f"
+  },
+  "/mille-sabords/css/settings.css": {
+    "version": "ca80b1f3",
+    "versionedUrl": "/mille-sabords/css/settings.css?v=ca80b1f3"
+  },
+  "/mille-sabords/other/wood2.png": {
+    "version": "15862f37",
+    "versionedUrl": "/mille-sabords/other/wood2.png?v=15862f37"
+  },
+  "/mille-sabords/other/wood.png": {
+    "version": "1edfbf4b",
+    "versionedUrl": "/mille-sabords/other/wood.png?v=1edfbf4b"
+  },
+  "/mille-sabords/other/stone.png": {
+    "version": "417f342b",
+    "versionedUrl": "/mille-sabords/other/stone.png?v=417f342b"
+  }
+};
+;(function(){var m = {"/mille-sabords/js/app_loader.nomodule.js":"/mille-sabords/js/app_loader.nomodule.js?v=fd8505e9","/mille-sabords/css/app.css":"/mille-sabords/css/app.css?v=31f65f42","/mille-sabords/js/app.nomodule.js":"/mille-sabords/js/app.nomodule.js?v=33630b7e","/mille-sabords/js/vendors.nomodule.js":"/mille-sabords/js/vendors.nomodule.js?v=6729057b","/mille-sabords/other/arrr_matey_bb_wn3.ttf":"/mille-sabords/other/arrr_matey_bb_wn3.ttf?v=3d20259e","/mille-sabords/other/card_small-1skull.png":"/mille-sabords/other/card_small-1skull.png?v=3c9f4a9f","/mille-sabords/other/card_small-2skulls.png":"/mille-sabords/other/card_small-2skulls.png?v=683eae22","/mille-sabords/other/card_small-sword-challenge.png":"/mille-sabords/other/card_small-sword-challenge.png?v=dc6ca6cf","/mille-sabords/other/card_small-animals.png":"/mille-sabords/other/card_small-animals.png?v=b931298f","/mille-sabords/other/card_small-chest.png":"/mille-sabords/other/card_small-chest.png?v=b171aea9","/mille-sabords/other/card_small-coin.png":"/mille-sabords/other/card_small-coin.png?v=a99cbd8e","/mille-sabords/other/card_small-diamond.png":"/mille-sabords/other/card_small-diamond.png?v=d3b35497","/mille-sabords/other/card_small-pirate.png":"/mille-sabords/other/card_small-pirate.png?v=ada07362","/mille-sabords/other/card_small-witch.png":"/mille-sabords/other/card_small-witch.png?v=5e50e07c","/mille-sabords/other/card_1skull.png":"/mille-sabords/other/card_1skull.png?v=47b3b46a","/mille-sabords/other/card_2skulls.png":"/mille-sabords/other/card_2skulls.png?v=61ce4149","/mille-sabords/other/card_2sword-challenge.png":"/mille-sabords/other/card_2sword-challenge.png?v=5c1924fd","/mille-sabords/other/card_3sword-challenge.png":"/mille-sabords/other/card_3sword-challenge.png?v=4097c59a","/mille-sabords/other/card_4sword-challenge.png":"/mille-sabords/other/card_4sword-challenge.png?v=f3927ba3","/mille-sabords/other/card_animals.png":"/mille-sabords/other/card_animals.png?v=e97f74ef","/mille-sabords/other/card_chest.png":"/mille-sabords/other/card_chest.png?v=037b957f","/mille-sabords/other/card_coin.png":"/mille-sabords/other/card_coin.png?v=19c1c721","/mille-sabords/other/card_default.png":"/mille-sabords/other/card_default.png?v=d777c0de","/mille-sabords/other/card_diamond.png":"/mille-sabords/other/card_diamond.png?v=ae0da860","/mille-sabords/other/card_pirate.png":"/mille-sabords/other/card_pirate.png?v=57c15344","/mille-sabords/other/card_witch.png":"/mille-sabords/other/card_witch.png?v=24c51955","/mille-sabords/other/dice_coin.png":"/mille-sabords/other/dice_coin.png?v=37896c9d","/mille-sabords/other/dice_diamond.png":"/mille-sabords/other/dice_diamond.png?v=6c36ed54","/mille-sabords/other/dice_monkey.png":"/mille-sabords/other/dice_monkey.png?v=044ce09c","/mille-sabords/other/dice_parrot.png":"/mille-sabords/other/dice_parrot.png?v=21f5c4b5","/mille-sabords/other/dice_skull.png":"/mille-sabords/other/dice_skull.png?v=271c6b85","/mille-sabords/other/dice_sword.png":"/mille-sabords/other/dice_sword.png?v=f0ac96f9","/mille-sabords/other/wood.jpg":"/mille-sabords/other/wood.jpg?v=3c23f120","/mille-sabords/other/pirate-hook.png":"/mille-sabords/other/pirate-hook.png?v=02fba0b6","/mille-sabords/other/pirate-hook-02.png":"/mille-sabords/other/pirate-hook-02.png?v=3db1b696","/mille-sabords/other/wood-box.jpg":"/mille-sabords/other/wood-box.jpg?v=fcdd41cc","/mille-sabords/other/treasure-map.png":"/mille-sabords/other/treasure-map.png?v=6ec97baa","/mille-sabords/other/witch-label.png":"/mille-sabords/other/witch-label.png?v=f95d3d2f","/mille-sabords/other/skull-bottle.png":"/mille-sabords/other/skull-bottle.png?v=7c833fb2","/mille-sabords/js/root.nomodule.js":"/mille-sabords/js/root.nomodule.js?v=a92bbf1e","/mille-sabords/js/jsenv_service_worker.js":"/mille-sabords/js/jsenv_service_worker.js?v=9047ac4f","/mille-sabords/css/dialog.base.css":"/mille-sabords/css/dialog.base.css?v=11b6a1ee","/mille-sabords/css/dialog.css":"/mille-sabords/css/dialog.css?v=3ddbc36c","/mille-sabords/other/cursed-grid.png":"/mille-sabords/other/cursed-grid.png?v=d5dc17c3","/mille-sabords/other/swords-disabled.png":"/mille-sabords/other/swords-disabled.png?v=124616f0","/mille-sabords/other/win-treasure.png":"/mille-sabords/other/win-treasure.png?v=7f39a7fa","/mille-sabords/other/boat.png":"/mille-sabords/other/boat.png?v=dc6ca6cf","/mille-sabords/other/Luffy.png":"/mille-sabords/other/Luffy.png?v=59728a08","/mille-sabords/other/miss_fortune.png":"/mille-sabords/other/miss_fortune.png?v=4b135ade","/mille-sabords/other/JackSparrow.png":"/mille-sabords/other/JackSparrow.png?v=f439b5cc","/mille-sabords/other/BarbeRouge.png":"/mille-sabords/other/BarbeRouge.png?v=b4a9d8e9","/mille-sabords/other/GinetteBoulette.png":"/mille-sabords/other/GinetteBoulette.png?v=de6a1ce1","/mille-sabords/other/CapitaineCrochet.png":"/mille-sabords/other/CapitaineCrochet.png?v=0ce0da4f","/mille-sabords/css/settings.css":"/mille-sabords/css/settings.css?v=ca80b1f3"}; self.__v__ = function (s) { return m[s] || s }; })();function ownKeys(e,r){var t=Object.keys(e);if(Object.getOwnPropertySymbols){var o=Object.getOwnPropertySymbols(e);r&&(o=o.filter((function(r){return Object.getOwnPropertyDescriptor(e,r).enumerable}))),t.push.apply(t,o)}return t}function _objectSpread(e){for(var r=1;r<arguments.length;r++){var t=null!=arguments[r]?arguments[r]:{};r%2?ownKeys(Object(t),!0).forEach((function(r){_defineProperty(e,r,t[r])})):Object.getOwnPropertyDescriptors?Object.defineProperties(e,Object.getOwnPropertyDescriptors(t)):ownKeys(Object(t)).forEach((function(r){Object.defineProperty(e,r,Object.getOwnPropertyDescriptor(t,r))}))}return e}function _defineProperty(e,r,t){return(r=_toPropertyKey(r))in e?Object.defineProperty(e,r,{value:t,enumerable:!0,configurable:!0,writable:!0}):e[r]=t,e}function _toPropertyKey(e){var r=_toPrimitive(e,"string");return"symbol"==typeof r?r:String(r)}function _toPrimitive(e,r){if("object"!=typeof e||null===e)return e;var t=e[Symbol.toPrimitive];if(void 0!==t){var o=t.call(e,r||"default");if("object"!=typeof o)return o;throw new TypeError("@@toPrimitive must return a primitive value.")}return("string"===r?String:Number)(e)}self.importScripts(__v__("/mille-sabords/js/jsenv_service_worker.js")),self.__sw__.init({name:"mille-sabords",resources:_objectSpread({"/jsenv-template-pwa/":{}},self.resourcesFromJsenvBuild||{})});
